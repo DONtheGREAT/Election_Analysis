fbf4ef1ccbefe58af8112c6fc22bffda19301b1d
Donald Graham <donaldg1994@yahoo.com>
2021-05-14 13:52:24 -0400

    present candidate votes

--- PyPoll.py
+++ PyPoll.py
@@ -14,6 +14,11 @@ candidate_options = []
 # Declare the empty dictionary {}
 candidate_votes = {}
 
+# Winning candidate and winning count tracker:
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file. From here on out until we make it back to this margin, the file to load will be read for everything below.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -41,6 +46,32 @@ with open(file_to_load) as election_data:
         #Add a vote to that candidates count. Pulled it one margin to remove it from the if statement. If not it will only count once since the if statement is only counting names once.
         candidate_votes[candidate_name] += 1
 
+# Determine the percentage of votes for each candidate by looping through the counts.
+# Iterate through the candidate list. Or for each name in the votes dictionary, do this -
+for candidate_name in candidate_votes:
+
+    # Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+
+    # Calculate the percentage of votes. use float() to turn the values into decimals before calculating percentage.
+    vote_percentage =float(votes) / float(total_votes) * 100
+
+    # To do: print out each candidates's name, vote count, and percentage of votes to the terminal.
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+    # Determine winning votes count and candidate
+    # Determine if the votes is greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        # If true then set winning_count = votes and winning_percentage = vote_percentage.
+        winning_count = votes
+        winning_percentage = vote_percentage
+        # and set the winning candidate equal to the candidates name.
+        winning_candidate = candidate_name
+
+    # Print the candidate name and percentage of votes.
+    print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
+
+    
 # Print the candidate list.
 print(candidate_votes)
 
